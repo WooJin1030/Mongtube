e963f2380ccdbf78c4c2ea4c410515bb5af0e6f0
Woojin <sam4woojin@naver.com>
2021-04-16 00:30:02 +0900

    route_for_home

--- src/server.js
+++ src/server.js
@@ -3,5 +3,15 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleListening = () => console.log(`Server listening on port ${PORT}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("<h1> get! </h1>");
+});
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
 app.listen(4000, handleListening);
