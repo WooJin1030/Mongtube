6c746570989954f7b1fef8d49b615fe70f684a70
Woojin <sam4woojin@naver.com>
2021-04-18 00:42:19 +0900

    init_controller

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { trending } from "../controllers/videoController";
+import { join } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
 
 export default globalRouter;
