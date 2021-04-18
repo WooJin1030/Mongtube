41ccaa82397c80d43bb91d09102e76c7b122c83e
Woojin <sam4woojin@naver.com>
2021-04-18 23:46:37 +0900

    ids_only_in_digit

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,7 @@ const userRouter = express.Router();
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/logout", logout);
+
 userRouter.get(":/id", see);
 
 export default userRouter;
