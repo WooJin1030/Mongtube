a17e23a615f9c98abc0d99c78249ed3e15b1c22a
Woojin <sam4woojin@naver.com>
2021-04-19 22:31:59 +0900

    home_findllvideos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,23 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+};
 
 export const watch = (req, res) => {
   const {
     params: { id },
   } = req;
   // console.log(id);
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
@@ -50,8 +25,6 @@ export const postEdit = (req, res) => {
     params: { id },
     body: { title },
   } = req;
-
-  videos[id - 1].title = title;
 
   return res.redirect(`/videos/${id}`);
 };
@@ -64,17 +37,6 @@ export const postUpload = (req, res) => {
   const {
     body: { title },
   } = req;
-
-  const newVideo = {
-    title: title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-
-  videos.push(newVideo);
 
   return res.redirect("/");
 };
