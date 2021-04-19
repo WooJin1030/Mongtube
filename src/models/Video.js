90969af7e51ff23b78a36a812b937d60ce6c9b3a
Woojin <sam4woojin@naver.com>
2021-04-19 21:59:26 +0900

    create_init.js

--- src/models/Video.js
+++ src/models/Video.js
@@ -12,5 +12,4 @@ const videoSchema = new mongoose.Schema({
 });
 
 const Video = mongoose.model("Video", videoSchema);
-
 export default Video;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -20,6 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✔️  Server listening on port http://localhost:${PORT}`);
-app.listen(4000, handleListening);
+export default app;
