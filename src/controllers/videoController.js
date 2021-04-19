553fc65fb97b3d92ea63099125719ca3194f05a2
Woojin <sam4woojin@naver.com>
2021-04-19 21:34:46 +0900

    fackDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -55,3 +55,26 @@ export const postEdit = (req, res) => {
 
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const {
+    body: { title },
+  } = req;
+
+  const newVideo = {
+    title: title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+
+  videos.push(newVideo);
+
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,8 @@ import {
   postEdit,
   upload,
   deleteVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -14,5 +16,7 @@ videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
+
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
