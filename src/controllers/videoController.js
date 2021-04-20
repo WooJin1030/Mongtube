2e7f475976130eba639232f9baf2189a414211c4
Woojin <sam4woojin@naver.com>
2021-04-20 14:42:07 +0900

    change_hashtags_dbmiddleware

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,9 +44,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((hashtag) => (hashtag.startsWith("#") ? hashtag : `#${hashtag}`)),
+    hashtags,
   });
 
   return res.redirect(`/videos/${id}`);
@@ -67,9 +65,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((hashtag) => (hashtag.startsWith("#") ? hashtag : `#${hashtag}`)),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,11 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((hashtag) => (hashtag.startsWith("#") ? hashtag : `#${hashtag}`));
+});
+
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
