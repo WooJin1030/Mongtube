a9948ed49062659d863ca61fdee15f7720dc65e9
Woojin <sam4woojin@naver.com>
2021-04-27 16:10:01 +0900

    join_view

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,21 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, email, usernam, password, location },
+  } = req;
+
+  await User.create({
+    name,
+    email,
+    usernam,
+    password,
+    location,
+  });
+
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 export const login = (req, res) => res.send("Login");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -13,7 +13,7 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
