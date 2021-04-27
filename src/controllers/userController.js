89ed835c4283e403b7ee8ffa111d0c9fd87eb41e
Woojin <sam4woojin@naver.com>
2021-04-27 23:24:33 +0900

    start_github_login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -73,6 +73,21 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = `https://github.com/login/oauth/authorize`;
+  const config = {
+    client_id: "5cfed0ea1885ecdb6400",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  console.log(params);
+  const finalUrl = `${baseUrl}?${params}`;
+  res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.get("/remove", remove);
+
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+
 userRouter.get("/logout", logout);
 
 userRouter.get(":/id", see);
