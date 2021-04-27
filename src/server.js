8a0d6362870412ead3f643051a61c0a2d12ed4cb
Woojin <sam4woojin@naver.com>
2021-04-27 21:47:03 +0900

    login_with_sessions

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares/middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +14,27 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// app.use((req, res, next) => {
+//   req.sessionStore.all((error, sessions) => {
+//     console.log(sessions);
+//     next();
+//   });
+// });
+
+app.use(localsMiddleware);
+
+// app.get("/add-one", (req, res, next) => {
+//   return res.send(`${req.session.id}`);
+// });
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
