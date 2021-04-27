25648610819b5fd575e4291b40780dcf756c7b3e
Woojin <sam4woojin@naver.com>
2021-04-27 21:55:02 +0900

    session_middleware

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,4 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
+  //console.log(req.session);
+  //console.log(req.sessionID);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
