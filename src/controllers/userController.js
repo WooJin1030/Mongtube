dec6d2275828cc82bf4c7b800652e7dae02f4947
Woojin <sam4woojin@naver.com>
2021-05-06 00:12:54 +0900

    change_some_css

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -158,7 +158,7 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
-  req.flash("info", "Bye Bye");
+  // req.flash("info", "Bye Bye");
   return res.redirect("/");
 };
 
