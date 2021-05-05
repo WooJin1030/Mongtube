7a1693017fd408a9ef043f06d8a33017b9800f73
Woojin <sam4woojin@naver.com>
2021-05-05 22:20:37 +0900

    ffmpeg_transcode_to_jpg

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -8,31 +8,68 @@ let stream;
 let recorder;
 let videoFile;
 
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
+  startBtn.removeEventListener("click", handleDownload);
+  startBtn.style.backgroundColor = "grey";
+  startBtn.innerText = "Transcoding...";
+  startBtn.disabled = true;
+
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
 
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  // transcoding (webm -> mp4)
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
+  // 동영상의 1초부분에서 이미지 캡쳐 (webm -> jpg)
+  await ffmpeg.run(
+    "-i",
+    files.input,
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    files.thumb
+  );
+
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+
   // console.log(mp4File.buffer);
-
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  // a.href = videoFile;
-  // a.download = "MyRecording.webm";
-  a.href = mp4Url;
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url, "MyRecording.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-  startBtn.innerText = "Show Record Screen";
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
+
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(thumbUrl);
+  URL.revokeObjectURL(videoFile);
+
+  startBtn.innerText = "RecordAgain";
   startBtn.style.backgroundColor = "black";
+  startBtn.disabled = false;
   video.src = null;
-
-  startBtn.removeEventListener("click", handleDownload);
   startBtn.addEventListener("click", handleCamera);
 };
 
