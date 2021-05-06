6bdb17e87ad9f216db34402d87d62541916414c1
Woojin <sam4woojin@naver.com>
2021-05-06 09:30:26 +0900

    create_comment_schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,6 +11,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, required: true, default: 0 },
     ratings: { type: Number, required: true, default: 0 },
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
