3e50c0f0dcda44491cfbafcc009cd31165bd86e1
Woojin <sam4woojin@naver.com>
2021-05-06 22:55:52 +0900

    video_duration_error

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -68,7 +68,9 @@ const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(11, 8);
 
 const handleLoadedMetadata = () => {
-  totalTime.innerText = formatTime(Math.floor(video.duration));
+  totalTime.innerText = video.duration;
+
+  // totalTime.innerText = formatTime(Math.floor(video.duration));
   timeLine.max = Math.floor(video.duration);
 };
 
