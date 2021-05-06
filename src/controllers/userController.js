26da47695b3290f0abd565b232457c6c083cb785
Woojin <sam4woojin@naver.com>
2021-05-06 21:30:47 +0900

    aws_s3_upload

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -190,7 +190,7 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,5 +1,19 @@
-import { request } from "express";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "mongtube",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   //console.log(req.session);
@@ -33,6 +47,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
@@ -40,4 +55,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
