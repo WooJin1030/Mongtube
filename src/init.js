dade296b8cf69459f4a3186fadb239fc9c289d00
Woojin <sam4woojin@naver.com>
2021-05-06 18:48:12 +0900

    server_port

--- src/init.js
+++ src/init.js
@@ -11,4 +11,4 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✔️  Server listening on port http://localhost:${PORT}`);
 
-app.listen(4000, handleListening);
+app.listen(4000, "0.0.0.0", handleListening);
